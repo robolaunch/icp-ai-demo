f32ad6b1ac9d08e807342f36a29da56f6155fd49
tunahanertekin <tunahan.ertekin13@gmail.com>
2024-05-30 12:59:01 +0300

    refactor(deepstream): migrate image assets from deepstream 6.4 to 7.0

--- artifacts/images/deepstream-recording/README.md
+++ artifacts/images/deepstream-recording/README.md
@@ -1,9 +1,9 @@
 # Build
 
 ```bash
-docker build -t robolaunchio/deepstream-recording:6.4-gc-triton-devel-v0.1.0 .
+docker build -t robolaunchio/deepstream-recording:7.0-gc-triton-devel-v0.1.0 .
 ```
 
 ```bash
-docker build -f Dockerfile.arm64 -t robolaunchio/deepstream-recording:6.4-gc-triton-devel-v0.1.0-arm64 .
+docker build -f Dockerfile.arm64 -t robolaunchio/deepstream-recording:7.0-gc-triton-devel-v0.1.0-arm64 .
 ```
--- artifacts/images/deepstream-recording/recording_setup.sh
+++ artifacts/images/deepstream-recording/recording_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # from deepstream-test5
-cd /opt/nvidia/deepstream/deepstream-6.4
+cd /opt/nvidia/deepstream/deepstream-7.0
 apt-get install -y \
     libgstreamer-plugins-base1.0-dev \
     libgstreamer1.0-dev \
@@ -11,7 +11,7 @@ apt-get install -y \
     libyaml-cpp-dev
 
 # from /opt/nvidia/deepstream/deepstream/sources/libs/kafka_protocol_adaptor
-cd /opt/nvidia/deepstream/deepstream-6.4
+cd /opt/nvidia/deepstream/deepstream-7.0
 git clone https://github.com/confluentinc/librdkafka.git
 cd librdkafka
 git checkout tags/v2.2.0
@@ -32,34 +32,34 @@ apt-get install -y \
     protobuf-compiler
     
 # from deepstream-test5
-cd /opt/nvidia/deepstream/deepstream-6.4/sources/apps/sample_apps/deepstream-test5
+cd /opt/nvidia/deepstream/deepstream-7.0/sources/apps/sample_apps/deepstream-test5
 export CUDA_VER=12.2
 make
 
-# cd /opt/nvidia/deepstream/deepstream-6.4
+# cd /opt/nvidia/deepstream/deepstream-7.0
 # git clone https://github.com/WongKinYiu/yolov7.git
 # git clone https://github.com/marcoslucianops/DeepStream-Yolo
-# cd /opt/nvidia/deepstream/deepstream-6.4/yolov7
+# cd /opt/nvidia/deepstream/deepstream-7.0/yolov7
 # pip3 install -r requirements.txt
 # pip3 install onnx onnxsim onnxruntime
 
-# cd /opt/nvidia/deepstream/deepstream-6.4/DeepStream-Yolo
+# cd /opt/nvidia/deepstream/deepstream-7.0/DeepStream-Yolo
 # CUDA_VER=12.2 make -C nvdsinfer_custom_impl_Yolo
 
 # # convert model
-# cd /opt/nvidia/deepstream/deepstream-6.4/yolov7
+# cd /opt/nvidia/deepstream/deepstream-7.0/yolov7
 # cp ../DeepStream-Yolo/utils/export_yoloV7.py .
 # wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
 # python3 export_yoloV7.py -w yolov7.pt --dynamic
 # cp labels.txt ../DeepStream-Yolo
 # cp yolov7.onnx ../DeepStream-Yolo
 
-# cd /opt/nvidia/deepstream/deepstream-6.4/sources/apps/sample_apps/deepstream-test5
+# cd /opt/nvidia/deepstream/deepstream-7.0/sources/apps/sample_apps/deepstream-test5
 
 # # move artifacts from DeepStream-YOLO repository to deepstream-test5
-# cp /opt/nvidia/deepstream/deepstream-6.4/DeepStream-Yolo/config_infer_primary_yoloV7.txt .
-# cp /opt/nvidia/deepstream/deepstream-6.4/DeepStream-Yolo/labels.txt .
-# cp /opt/nvidia/deepstream/deepstream-6.4/DeepStream-Yolo/yolov7.onnx .
-# cp -r /opt/nvidia/deepstream/deepstream-6.4/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/ .
+# cp /opt/nvidia/deepstream/deepstream-7.0/DeepStream-Yolo/config_infer_primary_yoloV7.txt .
+# cp /opt/nvidia/deepstream/deepstream-7.0/DeepStream-Yolo/labels.txt .
+# cp /opt/nvidia/deepstream/deepstream-7.0/DeepStream-Yolo/yolov7.onnx .
+# cp -r /opt/nvidia/deepstream/deepstream-7.0/DeepStream-Yolo/nvdsinfer_custom_impl_Yolo/ .
 
 echo -n \"--------------FINISHED--------------\"
